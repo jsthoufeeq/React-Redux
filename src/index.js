90e1825038a4f6445ccd713e544dcefdf0e1ab24
Thoufeeq Ahamed Jaffar Sadiq <jsthoufeeq@gmail.com>
2019-09-03 23:01:04 +0530

    code changes

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
+
+
+//react.js is a library
+
+
+// Front end - UI                   server side                          DB
+
+
+
+//html,css,jquery,bootstrap,DOJO,ex js, ajax, JS,sass,angular, react
+//Java, python, node, jsp, ROR, php, .net...
+//mongo, SQL, oacle, mySQL, postgre, access....
