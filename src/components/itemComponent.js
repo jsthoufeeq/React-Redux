21f09a28dda0e257c528abdbd314a677536817c4
Thoufeeq Ahamed Jaffar Sadiq <jsthoufeeq@gmail.com>
2019-09-06 08:41:49 -0700

    itmcomponent

--- src/components/itemComponent.js
+++ src/components/itemComponent.js
@@ -1,20 +1,57 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { addCounter, subCounter, changeName } from './../actions';
+import HeaderComponent from './headerComponent';
 
 class ItemComponent extends React.Component {
     constructor(props) {
         super(props);
-        console.log(props.match.params.id);
+        console.log(props);
     }
 
     componentWillUnmount() {
         localStorage.clear();
     }
 
+    incrementCounter = () => {
+        //todo: increment the counter value
+        console.log('coming inside handler');
+        this.props.dispatch(addCounter()) //dispatch an action
+    }
+
+    decrementCounter = () => {
+        this.props.dispatch(subCounter());
+    }
+
+    handleChange = (e) => {
+        this.props.dispatch(changeName(e.target.value));
+    }
+
     render() {
         return (
-            <div>This is Item component</div>
+            <div>
+                <h3>Counter demonstration</h3>
+                <HeaderComponent></HeaderComponent>
+                <p>count from item component is {this.props.count}</p>
+                <button className="btn btn-primary" onClick={this.incrementCounter}>Increment</button> <br />
+                <button className="btn btn-success" onClick={this.decrementCounter}>Decrement</button> <br />
+                <input type="text" onChange={this.handleChange}/>
+            </div>
         )
     }
 }
 
-export default ItemComponent;
+function mapDispatchToProps(dispatch) { //when we want to dispatch an action from the component
+    return {
+        actions: bindActionCreators(dispatch)
+    }
+}
+
+function mapStateToProps(state) {
+    return {
+        count: state.counterReducer
+    }
+}
+
+export default connect(mapDispatchToProps, mapStateToProps)(ItemComponent);
